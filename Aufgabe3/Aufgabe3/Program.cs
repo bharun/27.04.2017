6418beb9ec736fba66e6e1ce4a6b9cab29a5a0fa
bharun <harun.buljubasic@student.htldornbirn.at>
2017-04-27 20:09:28 +0200

    Add files via upload

---
+++ Aufgabe3/Aufgabe3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             double height, weight;
+             double bmi;
+             string input1, input2;
+             Console.Write("Enter your weight (kg): ");
+             input1 = Console.ReadLine();
+             weight = Convert.ToDouble(input1);
+
+             Console.Write("Enter your height (cm): ");
+             input2 = Console.ReadLine();
+             height = Convert.ToDouble(input2);
+
+            bmi = weight / ((height / 100) * (height / 100));
+            Console.WriteLine("Your BMI is: " + bmi.ToString("F"));
+            Console.ReadLine();
+
+             /*bmi = (double) weight / (double)((height/100) * (double)(height/100));
+             Console.WriteLine("Your BMI is: " + bmi);
+             Console.ReadLine();
+             */
+
+
+            /*double i = (double) 196 / 100;
+            Console.WriteLine(i);
+            Console.ReadLine();
+            */
+        }
+    }
+}
---
+++ Aufgabe6/Aufgabe6/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Aufgabe6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            listBox1.Items.Add(e.KeyCode);
+            StreamWriter sw = new StreamWriter(@"C:\Users\Harun\Documents\Visual Studio 2017\Projects\Aufgabe6\keyLog.txt", true);
+            sw.WriteLine(e.KeyCode);
+            sw.Close();
+        }
+    }
+}
